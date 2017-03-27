dd4b7c4e12e73c84d16911fe0346c509a9f283cb
betinasomer <betina.somer@gmail.com>
2017-03-27 13:01:03 -0300

    Classe subtração

---
+++ src/br/seminario/calculadora/Operacoes/Subtracao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.seminario.calculadora.Operacoes;
+
+/**
+ *
+ * @author Betina
+ */
+public class Subtracao {
+    private int a;
+    private int b;
+    private int resultado;
+    
+    public Subtracao() {
+        this.a = 0;
+        this.b = 0;
+        this.resultado = 0;
+    }
+    
+    public int subtrait(int x, int y){
+        return x-y;
+    }
+    
+    
+}
